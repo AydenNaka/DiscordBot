9776d091e4d786c6e0f31de9544db76d1c525e1a
DefinitelyNotAyden <146404323+DefinitlyNotAyden@users.noreply.github.com>
2025-08-31 04:19:49 -0700

    [0.1] Basic Code

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"initiated as {bot.user}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"synced {len(synced)} commands")
+    except Exception as e:
+        print(f"unable to sync {e}")
+
+@bot.tree.command(name="confess", description="send anonymous confession")
+@app_commands.describe(text="confession here")
+async def confess(interaction: discord.Interaction, text: str):
+    confession_channel = bot.get_channel(config.CONFESSION_CHANNEL_ID)
+    if not confession_channel:
+        await interaction.response.send_message("message cannot be blank", ephemeral=True)
+        return
+
+    await confession_channel.send(f"confession\n{text}")
+
+bot.run("demo")

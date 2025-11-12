4bb9fc875831478a99999002b3b8d2dff247497b
DefinitelyNotAyden <146404323+DefinitlyNotAyden@users.noreply.github.com>
2025-11-11 23:46:08 -0800

    added events

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
 # Approved Channels
 confession = 1390112594827677696
+
+# Users allowed for bot commands
+users = {
+    866113199811461170,
+    1438046356559101982
+}
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import random
+import json
+import datetime
+from datetime import date, timedelta
+
+eventdata = "eventdata.json"
+userdata = "data.json"
+
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -8,6 +16,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import bottoken
 
 intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -18,6 +27,35 @@ async def on_ready():
         print(f"synced {len(synced)} commands")
     except Exception as e:
         print(f"unable to sync {e}")
+
+@bot.event
+@commands.has_permissions(moderate_members=True)
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    
+    with open(eventdata, "r") as file:
+        active = json.load(file)
+    if date.fromisoformat(active["purge"]["enddate"]) > date.today():
+        with open(userdata, "r") as file:
+            userlist = json.load(file)
+        if str(message.author) not in userlist:
+            userlist[str(message.author)] = {
+                "purgelives": 0,
+                "badletters": 0,
+                "fineletters": 0
+            }
+        content = message.content.lower()
+        badletters = content.count(active["purge"]["letter"])
+        fineletters = len(content) - badletters
+        userlist[str(message.author)]["badletters"] = badletters
+        userlist[str(message.author)]["fineletters"] = fineletters
+        with open(userdata, "w") as file:
+            json.dump(userlist, file, indent=4)
+        if badletters > 0:
+            timeouttime = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=random.randint(1, 300))
+            await message.author.timeout(timeouttime, reason="You have spoken against god (Jackson) himself")
+            await message.add_reaction('<:banned:1438051448683495515>')
 
 @bot.tree.command(name="confess", description="send anonymous confession")
 @app_commands.describe(text="confession here")
@@ -30,5 +68,32 @@ async def confess(interaction: discord.Interaction, text: str):
 
     await confessionChannel.send(f"**confession**\n{text}")
     await interaction.followup.send("sent", ephemeral=True)
+
+@bot.tree.command(name="purge", description="initiate banned letter")
+async def purge(interaction: discord.Interaction):
+    if interaction.user.id in config.users:
+
+        # Generate New Letter
+        letters = "abcdefghijklmnopqrstuvwxyz"
+        with open(eventdata, "r") as file:
+            tempdata = json.load(file)
+        tempdata["purge"]["letter"] = letters[random.randint(0, len(letters) - 1)]
+        tempdata["purge"]["enddate"] = (date.today() + timedelta(days=1)).isoformat()
+        with open(eventdata, "w") as file:
+            json.dump(tempdata, file, indent=4)
+        
+        # Clear Past User Data
+        with open(userdata, "r") as file:
+            tempdata = json.load(file)
+        for key in tempdata:
+            tempdata[key]["badletters"] = 0
+            tempdata[key]["fineletters"] = 0
+        with open(userdata, "w") as file:
+            json.dump(tempdata, file, indent=4)
+
+        await interaction.response.send_message(
+            f"# The letter {tempdata['purge']['letter'].upper()} is banned for the next 24 hours",
+            ephemeral=True
+        )
 
 bot.run(bottoken.token)
